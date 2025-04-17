fa0f21359ad468642c01eb369de2390df1c02420
Mario Winter <mariowinter.sg@gmail.com>
2025-04-17 22:09:00 +0200

    feat(auth): integrate dj-rest-auth with email confirmation, JWT, and custom user registration flow

--- videoflix_app/urls.py
+++ videoflix_app/urls.py
@@ -1,35 +1,40 @@
-"""
-URL configuration for videoflix_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dj_rest_auth.registration.views import VerifyEmailView
+from allauth.account.views import ConfirmEmailView
 from user_auth_app.views import redirect_to_admin #redirect_to_schema
 #from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     # path('', redirect_to_schema, name='root'), only for production
+    
     path('', redirect_to_admin, name='root'),
     path('admin/', admin.site.urls),
     #path('api/', redirect_to_schema, name='root'), only for production
     path('api/', redirect_to_admin, name='root'),
-    path('api/', include('user_auth_app.api.urls')),
-    path('api-auth', include('rest_framework.urls')),
+    #path('api/', include('user_auth_app.api.urls')), für mögliches Profile Edite
     
+    #GET‑Bestätigung über Link in der E‑Mail
+    re_path(
+        r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$',
+        ConfirmEmailView.as_view(template_name='account/email_confirm.html'),
+        name='account_confirm_email'
+    ),
+    # Optional: Alias für dj-rest-auth VerifyEmailView
+    # path(
+    #     'api/auth/account-confirm-email/',
+    #     VerifyEmailView.as_view(),
+    #     name='account_email_verification_sent'
+    # ),
+    
+    # 1) dj-rest-auth Login/Logout/Password
+    path('api/auth/', include('dj_rest_auth.urls')),
+
+    # 2) dj-rest-auth Registration + E‑Mail‑Bestätigung (POST)
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+
     ## API Schema & Doku
     # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
