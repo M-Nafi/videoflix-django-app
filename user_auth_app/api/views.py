ead8e81c1e6f997cb394a2dd3be22c401d8c0d5d
Mario Winter <mariowinter.sg@gmail.com>
2025-06-22 12:47:11 +0200

    refactor(auth): simplify authentication setup by removing cookie-based JWT logic

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,87 +1,18 @@
-from django.conf import settings
-from django.shortcuts import redirect
-from rest_framework import viewsets, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import default_token_generator
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .serializers import UserSerializer
-from djoser.utils import decode_uid
 
 User = get_user_model()
 
-class FrontendActivationView(APIView):
-    """
-    Activate the user (if the token is valid), then redirect to admin.
-    """
-    authentication_classes = []   # allow anonymous GET
-    permission_classes = []       # allow anonymous GET
-
-    def get(self, request, uid: str, token: str):
-        try:
-            user_pk = decode_uid(uid)
-            user = User.objects.get(pk=user_pk)
-        except Exception:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-       
-        if default_token_generator.check_token(user, token):
-            user.is_active = True
-            user.save(update_fields=["is_active"])
-        return redirect(settings.FRONTEND_LOGIN_URL)
-
 
 class UserProfileViewSet(viewsets.ModelViewSet):
+    """
+    Retrieve and update the authenticated user's profile.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return User.objects.filter(id=self.request.user.id)
-
-class CookieTokenObtainPairView(TokenObtainPairView):
-    permission_classes = [AllowAny]
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        if response.status_code == 200:
-            data = response.data
-            response.set_cookie(
-                settings.SIMPLE_JWT['AUTH_COOKIE'], data['access'],
-                max_age=int(settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'].total_seconds()),
-                httponly=True, secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
-                samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
-            )
-            response.set_cookie(
-                settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'], data['refresh'],
-                max_age=int(settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'].total_seconds()),
-                httponly=True, secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
-                samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
-            )
-            response.data = {'detail': 'Cookies set'}
-        return response
-
-class CookieTokenRefreshView(TokenRefreshView):
-    permission_classes = [AllowAny]
-    def post(self, request, *args, **kwargs):
-        request.data['refresh'] = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
-        response = super().post(request, *args, **kwargs)
-        if response.status_code == 200:
-            data = response.data
-            response.set_cookie(
-                settings.SIMPLE_JWT['AUTH_COOKIE'], data['access'],
-                max_age=int(settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'].total_seconds()),
-                httponly=True, secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
-                samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
-            )
-            response.data = {'detail': 'Access cookie refreshed'}
-        return response
-
-class LogoutView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request):
-        response = Response(status=204)
-        response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE'])
-        response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
-        return response
